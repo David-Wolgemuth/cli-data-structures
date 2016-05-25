4e4c23c0abe1e0d246ac2ce62e0ca53890d261f7
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-05-25 12:58:29 -0700

    Array "partition" method

--- dataStructures/Sort.java
+++ dataStructures/Sort.java
@@ -4,12 +4,21 @@ import java.util.Arrays;
 
 public class Sort
 {
-    public static void main (String[] args)
+    public static <T extends Comparable<T>> void main (String[] args)
     {
-        Integer[] arr = new Integer[] { 8, 3, 4, 1, 0, 7, 5, 6, 9 };
-        System.out.print(Arrays.toString(arr));
-        Sort.insertion(arr);
-        System.out.println("Insertion Sorted: " + Arrays.toString(arr));
+        Integer[][] arrs = {
+            new Integer[] { 4, 3, 8, 1, 0, 7, 5, 6, 9 },
+            new Integer[] { 4, },
+            new Integer[] { 1, 3, 8, 1, 0, 7, 5, 6, 9 },
+            new Integer[] { 8, 3, 8, 1, 0, 7, 5, 6, 9 }
+        };
+        System.out.println("Partitioning Arrays:\n");
+        for (int i = 0; i < arrs.length; i++) {
+            System.out.println("Before: " + Arrays.toString(arrs[i]));
+            Sort.partition(arrs[i], 0, 0, arrs[i].length-1);
+            System.out.println("Partitioned: " + Arrays.toString(arrs[i]));
+            System.out.println("\n<-- ## -->\n");
+        }
     }
     public static <T extends Comparable<T>> void bubble (T[] arr)
     {
@@ -56,5 +65,24 @@ public class Sort
         T temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
+    }
+    public static <T extends Comparable<T>> void partition (T[] arr, int pivot, int start, int end)
+    {
+        if (start == end) {
+            return;
+        }
+        if (pivot != start) {
+            Sort.swap(arr, start, pivot);
+        }
+        pivot = start;
+        int last = pivot+1;
+        for (int i = last; i <= end; i++) {
+            if (arr[i].compareTo(arr[pivot]) >= 0) {
+                continue;
+            }
+            Sort.swap(arr, last, i);
+            last++; 
+        }
+        Sort.swap(arr, pivot, last-1);
     }
 }
