ad2a667a86c690c1a25adad327d215fb05770cea
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-05-26 13:45:30 -0700

    Quick Sort, and speed tests for sorting algorithms

--- dataStructures/Sort.java
+++ dataStructures/Sort.java
@@ -1,24 +1,13 @@
 package dataStructures;
 
 import java.util.Arrays;
+import java.util.Timer;
 
 public class Sort
 {
     public static <T extends Comparable<T>> void main (String[] args)
     {
-        Integer[][] arrs = {
-            new Integer[] { 4, 3, 8, 1, 0, 7, 5, 6, 9 },
-            new Integer[] { 4, },
-            new Integer[] { 1, 3, 8, 1, 0, 7, 5, 6, 9 },
-            new Integer[] { 8, 3, 8, 1, 0, 7, 5, 6, 9 }
-        };
-        System.out.println("Partitioning Arrays:\n");
-        for (int i = 0; i < arrs.length; i++) {
-            System.out.println("Before: " + Arrays.toString(arrs[i]));
-            Sort.partition(arrs[i], 0, 0, arrs[i].length-1);
-            System.out.println("Partitioned: " + Arrays.toString(arrs[i]));
-            System.out.println("\n<-- ## -->\n");
-        }
+        Sort.speed_test();
     }
     public static <T extends Comparable<T>> void bubble (T[] arr)
     {
@@ -60,17 +49,32 @@ public class Sort
             arr[insertionIndex] = pulled;
         }
     }
+    public static <T extends Comparable<T>> void quick (T[] arr)
+    {
+        Sort.quick(arr, 0, arr.length-1);
+    }
+    public static <T extends Comparable<T>> void quick (T[] arr, int start, int end)
+    {
+        int pivot = Sort.partition(arr, start, start, end);
+        if (pivot > start) {
+            Sort.quick(arr, start, pivot);
+        }
+        if (pivot < end) {
+            Sort.quick(arr, pivot+1, end);
+        }
+    }
     public static <T> void swap (T[] arr, int a, int b)
     {
         T temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-    public static <T extends Comparable<T>> void partition (T[] arr, int pivot, int start, int end)
+    public static <T extends Comparable<T>> int partition (T[] arr)
+    { 
+        return Sort.partition(arr, 0, 0, arr.length-1);
+    }
+    public static <T extends Comparable<T>> int partition (T[] arr, int pivot, int start, int end)
     {
-        if (start == end) {
-            return;
-        }
         if (pivot != start) {
             Sort.swap(arr, start, pivot);
         }
@@ -84,5 +88,54 @@ public class Sort
             last++; 
         }
         Sort.swap(arr, pivot, last-1);
+        return last-1;
+    }
+    private static void speed_test()
+    {
+        int size = 60 * 1000;
+        double time_start = System.nanoTime();
+        System.out.print("Create Array of " + size + " Random Integers: ");
+        Integer[] base_array = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            Integer num = (int)(Math.random() * size);
+            base_array[i] = num;
+        }
+        double time_end = System.nanoTime();
+        Sort.print_duration(time_start, time_end);
+
+        Integer[] arr = new Integer[size];
+
+        System.arraycopy(base_array, 0, arr, 0, size);
+        time_start = System.nanoTime();
+        System.out.print("Bubble Sort: ");
+        Sort.bubble(arr);
+        time_end = System.nanoTime();
+        Sort.print_duration(time_start, time_end);
+
+        System.arraycopy(base_array, 0, arr, 0, size);
+        time_start = System.nanoTime();
+        System.out.print("Selection Sort: ");
+        Sort.selection(arr);
+        time_end = System.nanoTime();
+        Sort.print_duration(time_start, time_end);
+
+        System.arraycopy(base_array, 0, arr, 0, size);
+        time_start = System.nanoTime();
+        System.out.print("Insertion Sort: ");
+        Sort.insertion(arr);
+        time_end = System.nanoTime();
+        Sort.print_duration(time_start, time_end);
+
+        System.arraycopy(base_array, 0, arr, 0, size);
+        time_start = System.nanoTime();
+        System.out.print("Quick Sort: ");
+        Sort.quick(arr);
+        time_end = System.nanoTime();
+        Sort.print_duration(time_start, time_end);
+    }
+    private static void print_duration (double start, double end)
+    {
+        double duration = (end - start) / 1000000000.0;
+        System.out.print(duration + " seconds\n");
     }
 }
